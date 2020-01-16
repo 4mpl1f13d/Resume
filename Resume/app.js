ee8f3e24d267ba398020f4e7cc28a5e4ecd308ee
4mpl1f13d <4mpl1f13d@users.noreply.github.com>
2020-01-16 11:19:24 -0600

    Add files via upload

--- Resume/app.js
+++ Resume/app.js
@@ -11,24 +11,26 @@ function displayPosition(coName, jobTitle, desc) {
 
 displayPosition(coName, jobTitle, desc);
 
-var theInt = '* TV (The best) \n* Computers \n* Movies... especially any Marvel / DC ports\n * Music\n';
+var theInt = '* TV (The best)\n* Computers\n* Movies... especially any Marvel / DC ports\n* Music\n';
 function myInterests() {
-    console.log('My Interests: \n', theInt);
+    console.log('My Interests:' + '\n', theInt);
 }
 
 myInterests();
 
-let prevExp = '* Carroll High School Graduate\n* Attended University of Alabama \n* Jan 2020 - Support.com - worked as Escalations Supervisor with Support HelpDesk for Central division \n* Jan 2017 - Alden Systems - worked as Software Support Analyst maintaining the external customer facing Help Desk \n* Jan 2010 - Code Red - Owned and operated my own electronic repair business \n* A+, Net+ experience'
+var prevExp = '* Carroll High School Graduate\n* Attended University of Alabama\n* Jan 2020 - Support.com - worked as Escalations Supervisor with Support HelpDesk for Central division\n* Jan 2017 - Alden Systems - worked as Software Support Analyst maintaining the external customer facing Help Desk\n* Jan 2010 - Code Red - Owned and operated my own electronic repair business\n* A+, Net+ experience'
 
 function myPreviousExperience() {
-    console.log('My Previous Experience: \n', prevExp);
+    console.log('My Previous Experience:\n', prevExp);
 }
 
 myPreviousExperience();
+
 console.log('My Skills: \n');
+
 function displaySkill(skillName, isCool) {
     if (isCool === true) {
-        console.log('BAM:' + skillName);
+        console.log('* BAM: ' + skillName);
     }
     else {
         console.log('* ' + skillName)
